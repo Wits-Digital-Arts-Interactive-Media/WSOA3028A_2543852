d448a05157e5fe70e9248dee81f0f6e7f0bfc893
JayQkew <rayjebb@gmail.com>
2024-05-13 22:00:11 +0200

    mouse invert!!

--- Scripts/cursorTrail.js
+++ Scripts/cursorTrail.js
@@ -1,3 +1,4 @@
+const coords = {x: 0, y: 0};
 const mouseTrailElements = [];
 
 function createTrailElements(num){
@@ -5,7 +6,36 @@ function createTrailElements(num){
         const circle = document.createElement("div");
         circle.setAttribute("class", "mouseTrail");
         mouseTrailElements.push(circle);
+        document.querySelector("body").appendChild(circle);
     }
 }
 
 createTrailElements(1);
+
+mouseTrailElements.forEach(function (circle){
+    circle.x = 0;
+    circle.y = 0;
+});
+
+window.addEventListener("mousemove", function(e){
+    coords.x = e.clientX;
+    coords.y = e.clientY;
+
+    animateTrail();
+});
+
+function animateTrail(){
+    let x = coords.x;
+    let y = coords.y;
+
+    mouseTrailElements.forEach(function (circle, index){
+        circle.style.left = x - 15 + "px";
+        circle.style.top = y - 15 + "px";
+        circle.x = x;
+        circle.y = y;
+
+        const nextCircle = mouseTrailElements[index + 1] || mouseTrailElements[0];
+        x += (nextCircle.x - x) * 0.25;
+        y += (nextCircle.y - y) * 0.25;
+    });
+}
