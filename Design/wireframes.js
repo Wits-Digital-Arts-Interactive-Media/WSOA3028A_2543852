0acbdf73e37c83194e3ea5ab4983292a8ea75326
JayQkew <rayjebb@gmail.com>
2024-05-25 23:14:46 +0200

    scrolling effect for cards

--- Design/wireframes.js
+++ Design/wireframes.js
@@ -8,4 +8,35 @@ function addIterationHeader(){
     }
 }
 
+const cardContainer = document.getElementsByClassName("cardContainer");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+for (const container of cardContainer) {
+    container.addEventListener('mousedown', (e) => {
+      isDown = true;
+      container.classList.add('active');
+      startX = e.pageX - container.offsetLeft;
+      scrollLeft = container.scrollLeft;
+    });
+    container.addEventListener('mouseleave', () => {
+      isDown = false;
+      container.classList.remove('active');
+    });
+    container.addEventListener('mouseup', () => {
+      isDown = false;
+      container.classList.remove('active');
+    });
+    container.addEventListener('mousemove', (e) => {
+      if(!isDown) return;
+      e.preventDefault();
+      const x = e.pageX - container.offsetLeft;
+      const walk = (x - startX) * 3; //scroll-fast
+      container.scrollLeft = scrollLeft - walk;
+      console.log(walk);
+    });
+    
+}
+
 addIterationHeader();
